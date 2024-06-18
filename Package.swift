dc0b382e76d894ab323f327a1a3e77b205ebeb91
HolidayBomb <mrducong@qq.com>
2024-06-18 17:45:06 +0800

    fix xml sdk not have  a info

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
         .library(name: "QCloudCOSXML", targets: ["QCloudCOSXML", "QCloudCore", "COSBeaconAPI_Base", "QimeiSDK"]),
     ],
     targets: [
-        .binaryTarget(name: "QCloudCOSXML", url: "https://github.com/freezy7/QCloudCOSXML/releases/download/6.4.3/QCloudCOSXML.xcframework.zip", checksum: "34b864468f48ffac9ee2bb10c50657b1b0be12df44fd795aef1f7f5f4d93644a"),
-        .binaryTarget(name: "QCloudCore", url: "https://github.com/freezy7/QCloudCOSXML/releases/download/6.4.3/QCloudCore.xcframework.zip", checksum: "ecc03b3cf4971cb1146c2b0410c70a1bcfbb9a8cf74564a7be56fe22498096cf"),
-        .binaryTarget(name: "COSBeaconAPI_Base", url: "https://github.com/freezy7/QCloudCOSXML/releases/download/6.4.3/COSBeaconAPI_Base.xcframework.zip", checksum: "e9414715d69a043b6dc5e42745bcfaeb67f7262806eace51855e1b5ac75aedf2"),
-        .binaryTarget(name: "QimeiSDK", url: "https://github.com/freezy7/QCloudCOSXML/releases/download/6.4.3/QimeiSDK.xcframework.zip", checksum: "d95dc3ecbfa3d606319e6919dd6af4bd5af0bc36577fd058eb33b0c5fc5c5af6")
+        .binaryTarget(name: "QCloudCOSXML", url: "https://github.com/freezy7/QCloudCOSXML/releases/download/6.4.4/QCloudCOSXML.xcframework.zip", checksum: "9eaeeec1796cacbe4eca281c388fb2891d686fad743a374ec0077e4383bde283"),
+        .binaryTarget(name: "QCloudCore", url: "https://github.com/freezy7/QCloudCOSXML/releases/download/6.4.4/QCloudCore.xcframework.zip", checksum: "ecc03b3cf4971cb1146c2b0410c70a1bcfbb9a8cf74564a7be56fe22498096cf"),
+        .binaryTarget(name: "COSBeaconAPI_Base", url: "https://github.com/freezy7/QCloudCOSXML/releases/download/6.4.4/COSBeaconAPI_Base.xcframework.zip", checksum: "e9414715d69a043b6dc5e42745bcfaeb67f7262806eace51855e1b5ac75aedf2"),
+        .binaryTarget(name: "QimeiSDK", url: "https://github.com/freezy7/QCloudCOSXML/releases/download/6.4.4/QimeiSDK.xcframework.zip", checksum: "d95dc3ecbfa3d606319e6919dd6af4bd5af0bc36577fd058eb33b0c5fc5c5af6")
     ]
 )
